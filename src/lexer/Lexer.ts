834672267ede8d45561b6d3b8fd1af1134727a85
Omar <omarfkuri@gmail.com>
2023-10-23 01:55:09 -0600

    Improved types, changed methods

--- src/lexer/Lexer.ts
+++ src/lexer/Lexer.ts
@@ -2,6 +2,9 @@ import { Char, CharList } from "./Char.js";
 import { Token, TokenList } from "./Token.js";
 import type { TknType, TagType } from "./types.js";
 
+/**
+ * Abstract class for converting source to token list
+ */
 export abstract class Lex<
   Type extends string,
   Tag extends string = string
@@ -9,6 +12,9 @@ export abstract class Lex<
 
 	#chars!: CharList
 
+  /**
+   * Convert string to TokenList
+   */
 	convert(source: string): TokenList<Type, Tag> {
 		this.#chars = new CharList(source);
 		const tokens = new TokenList<Type, Tag>();
@@ -23,34 +29,67 @@ export abstract class Lex<
 
 	protected abstract read(): Token<TknType<Type>, TagType<Tag>> | null;
 
+  /**
+   * Create a new Token
+   */
 	protected new(type: TknType<Type>, ...chars: Char[]): Token<TknType<Type>, TagType<Tag>> {
 		return new Token(type, ...chars)
 	}
 
+  /**
+   * @deprecated
+   * Use `willContinue`
+   */
 	protected finished(): boolean {
 		return this.#chars.length <= 0;
 	}
 
+  /**
+   * Whether there are `Char`s left
+   */
+  protected willContinue(): boolean {
+    return this.#chars.length > 0;
+  }
+
+  /**
+   * Get current `Char`
+   */
 	protected eat(): Char {
 		return this.#chars.shift()!;
 	}
 
+  /**
+   * Check if `Char` is string
+   */
 	protected is(str: string, n = 0): boolean {
 		return this.#chars[n]!.value === str;
 	}
 
+  /**
+   * Get current `Char` value
+   */
 	protected value(n = 0): string {
 		return this.#chars[n]!.value;
 	}
 
+  /**
+   * Check if `Char` matches regex
+   */
 	protected match(reg: RegExp, n = 0): boolean {
 		return reg.test(this.#chars[n]!.value);
 	}
 
+  /**
+   * @deprecated
+   * Use `is`
+   */
 	protected equal(str: string): boolean {
 		return this.#chars[0]!.value === str;
 	}
 
+  /**
+   * Check if `Char` is a letter
+   */
 	protected isLetter(): boolean {
 		const x = this.#chars[0]!.value;
 		return x.toUpperCase() !== x.toLowerCase();
